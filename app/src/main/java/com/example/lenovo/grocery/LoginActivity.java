22f6b0c869c6287375f14fe114fc5a5507c6df5b
RevathyRR <revathyramath@gmail.com>
2020-09-06 02:34:48 +0530

    volley & sharedpreference The app crashes

--- app/src/main/java/com/example/lenovo/grocery/LoginActivity.java
+++ app/src/main/java/com/example/lenovo/grocery/LoginActivity.java
@@ -1,29 +1,55 @@
 package com.example.lenovo.grocery;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import org.json.JSONArray;
+import java.util.HashMap;
+import java.util.Map;
+import app.AppController;
+
+
 
 public class LoginActivity extends AppCompatActivity {
     public Button but;
     public TextView sign;
+    public EditText email, password;
+    final String s1 = email.getText().toString().trim();
+    final String s2 = password.getText().toString().trim();
+    AlertDialogManager alert = new AlertDialogManager();
+    SessionManagement session;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        session = new SessionManagement(getApplicationContext());
 
-        but = findViewById(R.id.buttonContinue);
+        email = findViewById(R.id.editTextEmail);
+        password = findViewById(R.id.editTextPassword);
+        Toast.makeText(getApplicationContext(), "User Login Status: " + session.isLoggedIn(), Toast.LENGTH_LONG).show();
         sign = findViewById(R.id.signup);
+        but = findViewById(R.id.buttonContinue);
+
+
         but.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent sign = new Intent(LoginActivity.this,MainActivity.class);
-                startActivity(sign);
-                finish();
+                userLogin();
 
             }
         });
@@ -31,14 +57,68 @@ public class LoginActivity extends AppCompatActivity {
         sign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Intent sign = new Intent(LoginActivity.this,SignupActivity.class);
+                Intent sign = new Intent(LoginActivity.this, SignupActivity.class);
                 startActivity(sign);
                 finish();
-
-
             }
         });
 
     }
+
+
+    private void userLogin() {
+
+
+        if (TextUtils.isEmpty(s1)) {
+            email.setError("Please enter your email");
+            email.requestFocus();
+            return;
+        }
+
+
+        if (TextUtils.isEmpty(s2)) {
+            password.setError("Enter a password");
+            password.requestFocus();
+            return;
+        }
+
+
+        String tag_tag_arry = "json_array_req";
+        String url = "https://api.androidhive.info/volley/person_array.json";
+        final ProgressDialog pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Loading...");
+        pDialog.show();
+        JsonArrayRequest req =new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        //Log.d(TAG, response.toString());
+                        Toast.makeText(getApplicationContext(), "Done", Toast.LENGTH_SHORT).show();
+                        pDialog.hide();
+                        Intent sign = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(sign);
+                        finish();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //VolleyLog.e(TAG, "Error"+error.getMessage());
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+
+
+                pDialog.hide();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+               Map<String, String> params = new HashMap<String, String>();
+               params.put("email", "asd@gmail.com");
+               params.put("password", "password123");
+               return params;
+
+            }
+        };
+        AppController.getInstance().addToRequestQueue(req, tag_tag_arry);
+    }
+
 }
--- app/src/main/java/com/example/lenovo/grocery/MainActivity.java
+++ app/src/main/java/com/example/lenovo/grocery/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.lenovo.grocery;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+    AlertDialogManager alert = new AlertDialogManager();
+    SessionManagement session;
+    Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        session = new SessionManagement(getApplicationContext());
+
+        TextView lblPass = (TextView) findViewById(R.id.lblPass);
+        TextView lblEmail = (TextView) findViewById(R.id.lblEmail);
+
+        // Button logout
+        btnLogout = (Button) findViewById(R.id.btnLogout);
+
+        Toast.makeText(getApplicationContext(), "User Login Status: " + session.isLoggedIn(), Toast.LENGTH_LONG).show();
+        session.checkLogin();
+        HashMap<String, String> user = session.getUserDetails();
+        String email = user.get(SessionManagement.KEY_EMAIL);
+        String password = user.get(SessionManagement.KEY_PASS);
+        lblEmail.setText(Html.fromHtml("Email: <b>" + email + "</b>"));
+        lblPass.setText(Html.fromHtml("Password: <b>" + password + "</b>"));
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // Clear the session data
+                // This will clear all session data and
+                // redirect user to LoginActivity
+                session.logoutUser();
+            }
+
+
+        });
     }
 }
